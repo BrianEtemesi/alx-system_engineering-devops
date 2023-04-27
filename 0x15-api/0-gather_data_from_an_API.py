612ea0acc641ed7356d9e138ad24ee63557bc8ac
BrianEtemesi <etemecci@gmail.com>
2023-04-27 20:45:11 +0000

    gather data from API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Given an employee ID, returns TODO info from a REST API
+"""
+
+import requests
+import json
+from sys import argv
+
+if __name__ == "__main__":
+
+    # get user object to obtain user attributes
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+    res = requests.get(url)
+    name = res.json().get('name')
+
+    # get todo list object associated with user
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1])
+    res = requests.get(url)
+    to_json = res.json()
+
+    # calculate number of completed tasks
+    done = 0
+    for task in to_json:
+        if task['completed'] is True:
+            done += 1
+
+    # total number of tasks
+    total = len(to_json)
+
+    print("Employee {} is done with tasks({}/{}):".format(name, done, total))
+
+    # print title of completed tasks
+    for task in to_json:
+        if task['completed'] is True:
+            print("\t {}".format(task['title']))
