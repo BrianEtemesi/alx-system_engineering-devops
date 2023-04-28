0fc936f6ec6c573cdbcb0f0ea94d5a7b7907f209
BrianEtemesi <etemecci@gmail.com>
2023-04-28 10:06:55 +0000

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Given an employee ID, returns TODO info from a REST API and
+copies the data to a CSV file
+"""
+
+import csv
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    # get user object to obtain user attributes
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+    res = requests.get(url)
+    name = res.json().get('name')
+
+    # get todo list object associated with user
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1])
+    res = requests.get(url)
+    to_json = res.json()
+
+    # create a CSV file with format USER_ID.csv
+    file_name = "{}.csv".format(argv[1])
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+
+        # copy data to CSV file
+        for task in to_json:
+            writer.writerow([argv[1], name, task['completed'], task['title']])
